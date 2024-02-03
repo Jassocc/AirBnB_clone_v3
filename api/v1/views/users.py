e7bf84f67e18664b9f2f9baa9d1c7a886e1d4aac
opiyoxbenji <opiyosbenji@gmail.com>
2024-02-03 16:19:40 +0000

    updated the user object again

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,7 +9,8 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'],
+strict_slashes=False)
 def get_users():
     """
     Gets the list of all user objects
@@ -19,7 +20,8 @@ def get_users():
     return jsonify(user_list)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'],
+strict_slashes=False)
 def get_user(user_id):
     """
     fetches a user object
@@ -30,7 +32,8 @@ def get_user(user_id):
     abort(404)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+strict_slashes=False)
 def delete_user(user_id):
     """
     deletes users by id
@@ -43,7 +46,8 @@ def delete_user(user_id):
     abort(404)
 
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users', methods=['POST'],
+strict_slashes=False)
 def create_user():
     """
     creates a user
@@ -61,7 +65,8 @@ def create_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'],
+strict_slashes=False)
 def update_user(user_id):
     """
     updates a user object
