9c0dce06dc915fef9ef0464604840da68383994a
Jassocc <clnearle@gmail.com>
2024-02-03 12:01:16 +0000

    udpate

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,15 @@
 """
 index py file
 """
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -21,17 +21,17 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """
     retrieves the number of objects
     """
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
+    stat = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
     }
-    return jsonify(stats)
+    return jsonify(stat)
