4ec5e695d9bbd09ea6285ef0909b293c79a599d1
Jassocc <clnearle@gmail.com>
2024-02-04 09:12:24 +0000

    update

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,14 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if kwargs.get('password'):
+            self.password = md5(kwargs['password'].encode()).hexdigest()
+
+    def to_dict(self, save_password=False):
+        """
+        returns a dictionary containing all keys
+        """
+        new_dict = super().to_dict()
+        if not save_password and 'password' in new_dict:
+            del new_dict['password']
+        return new_dict
