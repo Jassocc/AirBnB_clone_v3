8ed2a2579fbd36e09d8fb06cfb80abe501323cda
Jassocc <clnearle@gmail.com>
2024-02-04 08:15:05 +0000

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -24,7 +26,7 @@ def not_found(error):
     """
     handles 404 errors and return a response
     """
-    return (jsonify({'error': 'Not Found'}), 404)
+    return (jsonify({"error": "Not Found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
